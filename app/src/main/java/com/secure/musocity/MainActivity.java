a87daa8e69b707f9d31263f89c91534e3ac267f5
SatyamSoni23 <2018kucp1121@iiitkota.ac.in>
2020-08-23 12:25:54 +0530

    frontend

--- app/src/main/java/com/secure/musocity/MainActivity.java
+++ app/src/main/java/com/secure/musocity/MainActivity.java
@@ -1,8 +1,15 @@
 package com.secure.musocity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init_view_pager();
+    }
+
+    private void init_view_pager() {
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+    }
+
+    public static class ViewPagerAdapter extends FragmentPagerAdapter{
+
+        public ViewPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
     }
 }
