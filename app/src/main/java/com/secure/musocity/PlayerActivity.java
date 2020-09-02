930ab5f8ef2f801b5678ddabbe5974478d33a1b1
SatyamSoni23 <2018kucp1121@iiitkota.ac.in>
2020-09-02 16:20:53 +0530

    player activity

---
+++ app/src/main/java/com/secure/musocity/PlayerActivity.java
@@ -0,0 +1,14 @@
+package com.secure.musocity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
