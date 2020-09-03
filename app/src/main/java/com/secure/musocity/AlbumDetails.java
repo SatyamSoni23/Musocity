ebda2bcc5a2a5b4336459195308f88160ee53648
SatyamSoni23 <2018kucp1121@iiitkota.ac.in>
2020-09-04 00:46:17 +0530

    add album activity

---
+++ app/src/main/java/com/secure/musocity/AlbumDetails.java
@@ -0,0 +1,73 @@
+package com.secure.musocity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.media.MediaMetadataRetriever;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import static com.secure.musocity.MainActivity.musicFiles;
+
+public class AlbumDetails extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ImageView albumPhoto;
+    String albumName;
+    ArrayList<MusicFiles> albumSongs = new ArrayList<>();
+    AlbumDetailsAdapter albumDetailsAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album_details);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        albumPhoto = findViewById(R.id.albumPhoto);
+        albumName = getIntent().getStringExtra("albumName");
+        int j = 0;
+        for(int i=0; i<musicFiles.size(); i++){
+            if(albumName.equals(musicFiles.get(i).getAlbum())){
+                albumSongs.add(j, musicFiles.get(i));
+                j++;
+            }
+        }
+        byte[] image = getAlbumArt(albumSongs.get(0).getPath());
+        if(image != null){
+            Glide.with(this)
+                    .load(image)
+                    .into(albumPhoto);
+        }
+        else{
+            Glide.with(this)
+                    .load(R.drawable.blank_img)
+                    .into(albumPhoto);
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!(albumSongs.size() < 1)){
+            albumDetailsAdapter = new AlbumDetailsAdapter(this, albumSongs);
+            recyclerView.setAdapter(albumDetailsAdapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this,
+                    RecyclerView.VERTICAL, false));
+        }
+    }
+
+    private byte[] getAlbumArt(String uri){
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(uri);
+        byte[] art = retriever.getEmbeddedPicture();
+        retriever.release();
+        return art;
+    }
+
+}
---
+++ app/src/main/java/com/secure/musocity/albumAdapter.java
@@ -0,0 +1,81 @@
+package com.secure.musocity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaMetadataRetriever;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class albumAdapter extends RecyclerView.Adapter<albumAdapter.MyHolder> {
+    private Context mContext;
+    private ArrayList<MusicFiles> albumsFiles;
+    View view;
+
+    public albumAdapter(Context mContext, ArrayList<MusicFiles> albumsFiles) {
+        this.mContext = mContext;
+        this.albumsFiles = albumsFiles;
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        view = LayoutInflater.from(mContext).inflate(R.layout.album_item, parent, false);
+        return new MyHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, final int position) {
+        holder.album_name.setText(albumsFiles.get(position).getAlbum());
+        byte[] image = getAlbumArt(albumsFiles.get(position).getPath());
+        if(image != null){
+            Glide.with(mContext).asBitmap()
+                    .load(image)
+                    .into(holder.album_image);
+        }
+        else{
+            Glide.with(mContext)
+                    .load(R.drawable.blank_img)
+                    .into(holder.album_image);
+        }
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, AlbumDetails.class);
+                intent.putExtra("albumName", albumsFiles.get(position).getAlbum());
+                mContext.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return albumsFiles.size();
+    }
+
+    public class MyHolder extends RecyclerView.ViewHolder {
+        ImageView album_image;
+        TextView album_name;
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+            album_image = itemView.findViewById(R.id.album_image);
+            album_name = itemView.findViewById(R.id.album_name);
+        }
+    }
+    private byte[] getAlbumArt(String uri){
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(uri);
+        byte[] art = retriever.getEmbeddedPicture();
+        retriever.release();
+        return art;
+    }
+}
